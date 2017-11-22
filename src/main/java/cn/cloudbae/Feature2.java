e68b6696d1406057c5d83d52c27aa3527cd3a1b6
袁军 <yuanjun4618@dingtalk.com>
2017-11-22 11:34:09 +0800

    feature2提交

---
+++ src/main/java/cn/cloudbae/Feature2.java
@@ -0,0 +1,11 @@
+package cn.cloudbae;
+
+public class Feature2 {
+
+	//仅供测试分支回滚
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Feature2");
+	}
+
+}
